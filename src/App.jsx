13cd40f37b97679f3d55e66d62a84204e9edbfb1
Saurabh negi <negis9705@gmail.com>
2025-02-12 11:28:08 +0530

    add import theme

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import theme from './theme/theme';
 import AppRoutes from './routes/AppRoutes';
-
+import theme from './theme/theme';
 function App() {
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
         <Router>
           <AppRoutes />
         </Router>
-    </ThemeProvider>
+    </ThemeProvider>   
+
   );
 }
 
