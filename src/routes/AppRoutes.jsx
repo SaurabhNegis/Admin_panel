afde9b8c54d5ae0c15ee471e928b270822b732bb
Saurabh negi <negis9705@gmail.com>
2025-02-12 11:31:08 +0530

    fixed routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,4 +1,5 @@
 // src/routes/AppRoutes.jsx
+
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Dashboard from '../pages/Dashboard';
@@ -10,21 +11,26 @@ import Logout from '../pages/Logout';
 import MainLayout from '../layout/MainLayout';
 
 const AppRoutes = () => (
-
   <Routes>
+    {/* Route for Login Page - accessible without authentication */}
     <Route path="/login" element={<Login />} />
     <Route path="/logout" element={<Logout />} />
 
+    {/* Protected routes wrapped inside MainLayout */}
     <Route path="/" element={<MainLayout />}>
+      {/* Default route (Dashboard) when visiting "/" */}
       <Route index element={<Dashboard />} />
+
+      {/* Route for Users page */}
       <Route path="users" element={<Users />} />
+
+      {/* Route for Settings page */}
       <Route path="settings" element={<Settings />} />
     </Route>
 
+    {/* Uncomment this to handle unknown routes with a NotFound page */}
     {/* <Route path="*" element={<NotFound />} /> */}
   </Routes>
-
-
 );
 
 export default AppRoutes;
