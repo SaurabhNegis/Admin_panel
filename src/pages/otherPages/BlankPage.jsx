e693924e46506c29bd2a7b986860a1814aa6962a
Saurabh negi <negis9705@gmail.com>
2025-02-17 11:52:56 +0530

    create BlackPage

---
+++ src/pages/otherPages/BlankPage.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Box } from '@mui/material';
+
+export default function BlankPageWithLayout() {
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        height: '100vh',
+        backgroundColor: 'white', // Set the background to white or any other color you prefer
+      }}
+    />
+  );
+}
